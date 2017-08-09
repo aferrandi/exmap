e5d06ed330654c593a0df5b1c4c5f2392a3f2b17
Andrea Ferrandi <ferrandi.andrea@gmail.com>
2017-08-09 18:53:50 +0200

    better handling of io exceptions when loading files

--- server/src/Main.hs
+++ server/src/Main.hs
@@ -2,18 +2,24 @@ module Main where
 
 import System.Environment
 
-import qualified WebApp
-import qualified Data.Maybe as M
+import qualified Data.Maybe as B
+import qualified Data.Map.Strict as M
+import Control.Concurrent.STM.TVar
 
+import State
 import StateBuild
+import qualified WebApp
 
 
 main :: IO ()
 main = do
     args <- getArgs
-    case M.listToMaybe args of
+    case B.listToMaybe args of
         Just root -> do
-            let system = startSystem root
+            print $ "loading system from " ++ root
+            system <- startSystem root
+            ps <- readTVarIO (projectByName system)
+            print $ "System loaded with " ++ show (M.size ps) ++ " projects"
             WebApp.runWebApp
         Nothing -> do
             print "exmap <rootPath>"
--- server/src/storage/Load.hs
+++ server/src/storage/Load.hs
@@ -4,6 +4,7 @@ import Data.Aeson
 import System.IO
 import System.IO.Error
 import System.FilePath
+import Control.Exception
 import qualified Data.ByteString.Lazy as B
 
 import Paths
@@ -11,17 +12,27 @@ import XMapTypes
 import Project
 import ProjectJson
 
+tryReadFile :: FilePath -> IO (Either IOError B.ByteString)
+tryReadFile p = try $ B.readFile p
+
 loadAvailableProjects :: FilePath -> IO (Either String AllProjects)
 loadAvailableProjects root = do
-     json <- B.readFile $ allProjectsPath root
-     return $ eitherDecode json
+     strOrExc <- tryReadFile (allProjectsPath root)
+     return $ case strOrExc of
+        Left ex -> Left (show ex)
+        Right json -> eitherDecode json
+
 
 loadProject :: FilePath -> ProjectName -> IO (Either String Project)
 loadProject root pr = do
-    json <- B.readFile $ projectPath root pr
-    return $ eitherDecode json
+     strOrExc <- tryReadFile (projectPath root pr)
+     return $ case strOrExc of
+        Left ex -> Left (show ex)
+        Right json -> eitherDecode json
 
 loadXMap :: FilePath -> XMapName -> IO (Either String XNamedMap)
 loadXMap root m = do
-    json <- B.readFile $ xMapPath root m
-    return $ eitherDecode json
+     strOrExc <- tryReadFile (xMapPath root m)
+     return $ case strOrExc of
+        Left ex -> Left (show ex)
+        Right json -> eitherDecode json
