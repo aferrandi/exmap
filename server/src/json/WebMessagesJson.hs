f7a51adc21d8d84224d9dceec8d67ee83b94bbfb
Andrea Ferrandi <ferrandi.andrea@gmail.com>
2017-10-07 15:11:49 +0200

    missing files

--- server/src/json/WebMessagesJson.hs
+++ server/src/json/WebMessagesJson.hs
@@ -63,5 +63,5 @@ instance FromJSON WebRequest where
       Just (String "storeMap") ->  WRStoreMap <$> v .: "projectName" <*> v .: "map"
       Just (String "subscribeToView") -> WRSubscribeToView <$> v .: "projectName" <*> v .: "viewName"
       Just (String "unsubscribeFromView") ->  WRUnsubscribeFromView <$> v .: "projectName" <*> v .: "viewName"
-      otherwise -> mempty
+      _ -> mempty
    parseJSON _ = mempty
