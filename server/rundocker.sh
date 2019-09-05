74ec229e1573e142ea66b24b8db74279c6c9afb2
andrea <ferrandi.andrea@gmail.com>
2019-09-05 19:43:25 +0200

    updated rundocker.sh with the port

--- server/rundocker.sh
+++ server/rundocker.sh
@@ -1,1 +1,1 @@
-docker run -v ~/prj/exmap/server/systemdirtest:/etc/prjroot  -p 5000:3000 aferrandi/exmap exmap /etc/webclient/index.html /etc/prjroot
+docker run -v ~/prj/exmap/server/systemdirtest:/etc/prjroot  -p 5000:3000 aferrandi/exmap exmap 3000 /etc/webclient/index.html /etc/prjroot
