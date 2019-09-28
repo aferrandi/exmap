9198aea6bf4f5d8967d76c85c9c1fd0b9ccb1054
andrea <ferrandi.andrea@gmail.com>
2019-09-28 21:54:45 +0200

    changed the docker port to 5000

--- server/rundocker.sh
+++ server/rundocker.sh
@@ -1,3 +1,3 @@
 EXDIR=/opt/executable
 
-docker run -v ~/prj/exmap/server/systemdirtest:$EXDIR/prjroot  -p 3000:3000 aferrandi/exmap $EXDIR/exmap 3000 $EXDIR/webclient $EXDIR/prjroot
+docker run -v ~/prj/exmap/server/systemdirtest:$EXDIR/prjroot  -p 5000:3000 aferrandi/exmap $EXDIR/exmap 3000 $EXDIR/webclient $EXDIR/prjroot
